ddd8dc6d0150495bb5d5f83e866311708a05f649
limadejuju <lima.juliano30@escola.pr.gov.br>
2025-08-14 07:42:34 -0300

    1

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
   const texto = document.querySelector("#entrada-de-texto").value;
-  const campoResultado = document.querySelector("#resultado-palavrachave");
-  const palavras = texto.split(" ");
 
-  campoResultado.textContent = palavras;
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavrasChave = processaTexto(texto);
+
+  campoResultado.textContent = palavrasChave.join(", ");
 }
- 
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  return palavras;
+}
