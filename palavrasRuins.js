fc09246abb0cd786acbf44b4654e15b3cded7004
limadejuju <lima.juliano30@escola.pr.gov.br>
2025-08-14 07:59:33 -0300

    11

---
+++ palavrasRuins.js
@@ -0,0 +1,167 @@
+export const PALAVRAS_RUINS = new Set([
+    "que",
+    "para",
+    "com",
+    "não",
+    "uma",
+    "por",
+    "mais",
+    "dos",
+    "como",
+    "mas",
+    "foi",
+    "ele",
+    "das",
+    "tem",@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+:root {
+    --cor-destaque: #ED145B;
+    --cor-gradiente-preto: linear-gradient(113deg, #000 0%, #1E2021 44.96%, #1E2022 100%);
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    font-family: Montserrat;
+}
+
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 16px 21px 18px 21px;
+    background: var(--cor-gradiente-preto);
+    height: 100vh;
+    flex-direction: column;
+}
+
+
+.container {
+    display: flex;
+    width: 65rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 3rem;
+    overflow: hidden;
+}
+
+h1 {
+    color: var(--cor-destaque);
+    text-align: center;
+    font-size: 2.8rem;
+    font-weight: 600;
+    text-transform: uppercase;
+}
+
+#entrada-de-texto {
+    display: flex;
+    height: 12rem;
+    "seu",
+    "sua",
+    "ser",
+    "quando",
+    "muito",
+    "está",
+    "também",
+    "pelo",
+    "pela",
+    "até",
+    "isso",
+    "ela",
+    "entre",
+    "era",
+    "depois",
+    "sem",
+    "mesmo",
+    "aos",
+    "ter",
+    "seus",
+    "quem",
+    "nas",
+    "esse",
+    "eles",
+    "estão",
+    "você",
+    "tinha",
+    "foram",
+    "essa",
+    "num",
+    "nem",
+    "suas",
+    "meu",
+    "minha",
+    "têm",
+    "numa",
+    "pelos",
+    "elas",
+    "havia",
+    "seja",
+    "qual",
+    "será",
+    "nós",
+    "tenho",
+    "lhe",
+    "deles",
+    "essas",
+    "esses",
+    "pelas",
+    "este",
+    "fosse",
+    "dele",
+    "vocês",
+    "vos",
+    "lhes",
+    "meus",
+    "minhas",
+    "teu",
+    "tua",
+    "teus",
+    "tuas",
+    "nosso",
+    "nossa",
+    "nossos",
+    "nossas",
+    "dela",
+    "delas",
+    "esta",
+    "estes",
+    "estas",
+    "aquele",
+    "aquela",
+    "aqueles",
+    "aquelas",
+    "isto",
+    "aquilo",
+    "estou",
+    "estamos",
+    "estive",
+    "esteve",
+    "estivemos",
+    "estiveram",
+    "estava",
+    "estávamos",
+    "estavam",
+    "e",
+    "ou",
+    "onde",
+    "aquilo",
+    "embora",
+    "apesar",
+    "porque",
+    "enquanto",
+    "contudo",
+    "entretanto",
+    "portanto",
+    "além",
+    "antes",
+    "todavia",
+    "inclusive",
+    "diante",
+    "sobre",
+    "quanto",
+    "apenas",
+    "desde",
+    "uma vez",
+    "segundo",
+    "junto"
+]);
